a7d084724d399f8dd5183d93893fec36180fb2bb
Jess-33 <jesilescano8532@gmail.com>
2026-08-23 11:19:44 +0200

    modifique 3,4 y 6

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ def numeros_impares_juntos(entrada):
         if numero % 2 != 0:
             impares.append(str(numero))
     return ", ".join(impares)
-
+print(f'\n----EJERCICIO 3----\n')
 print(numeros_impares_juntos([1, 2, 3, 4, 5, 6, 7, 22,31]))
 
 #Ejercicio 4
+print(f'\n----EJERCICIO 4----\n')
 def  lista_elementos_en_comun(lista1, lista2):
     elementos_comunes = []
     for elemento in lista1:
         if elemento in lista2 and elemento not in elementos_comunes:
             elementos_comunes.append(elemento)
     for elemento in elementos_comunes:
-        print(elemento)        
+        print(elemento)   
 lista1 = [1, 2, 3, 4, 5]
 lista2 = [1, 3, 6, 7, 8]
 lista_elementos_en_comun(lista1, lista2)
 
 #Ejercicio 6
+print(f'\n----EJERCICIO 6----\n')
 def persona_mayor_de_edad(edad):
     return edad >= 18
 print(persona_mayor_de_edad(20))
